7fb33154bf86b46daf8d9c1486e4dc07ffe4c131
laviphon <laviphon@gmail.com>
2025-05-02 18:53:16 -0400

    archon in the real world

--- lua/custom/laviphon/init.lua
+++ lua/custom/laviphon/init.lua
@@ -20,3 +20,24 @@ map('n', '<leader>x', '<Cmd>BufferClose<CR>', opts)
 -- nvim tree map
 
 map('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>', opts)
+
+local api = require 'image'
+api.enable()
+local dashboardImage = vim.api.nvim_create_augroup('DashboardImage', { clear = true })
+local img = api.from_file('/home/laviphon/Downloads/rwa_logo_static_v1.png', {
+  width = 50,
+  height = 50,
+})
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  group = dashboardImage,
+  callback = function()
+    img:move(78, 1)
+    img:render()
+  end,
+})
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  group = dashboardImage,
+  callback = function()
+    img:clear()
+  end,
+})
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,4 +1,3 @@
-require 'custom.laviphon.ascii'
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
@@ -42,10 +41,29 @@ return {
         -- config
         shortcut_type = 'number',
         config = {
-          header = AsciiHeaderArchon,
+          header = {
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+          },
         },
       }
     end,
     dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
+
+  {
+    '3rd/image.nvim',
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      processor = 'magick_cli',
+    },
   },
 }
